d28efbb4b51179f6f27f2588c1d5cd26fed18058
karam <karamkaku2000@gmail.com>
2022-05-18 16:30:11 +0200

    fix a bug

--- components/common/layout/header/index.tsx
+++ components/common/layout/header/index.tsx
@@ -11,20 +11,6 @@ const Header = ({nav})=>{
 			  {nav.map((navItem,idx)=>(
 			  <Link href={`/#${navItem}`} key={idx}>                               
 <a className="text-color-secondary dark:text-color-third font-thin  uppercase text-[0.8rem] tracking-widest relative before:content-[''] after:content-[''] before:w-0  after:w-0 before:h-[6px] after:bg-color-secondary after:absolute after:h-[6px] before:bg-color-secondary before:absolute before:bottom-[-100%] after:bottom-[-100%] before:left-[50%] before:right-[50%] after:left-[50%] after:right-[50%] before:rounded-[20px] after:rounded-[20px] before:transition-all before:ease-in-out before:duration-500  after:transition-all after:ease-in-out after:duration-500  hover:before:w-[100%] hover:after:w-[50%] hover:before:left-0 " >
-
-
-
-
-
-
-
-	  
-
-
-	  
-
-
-
 				 {navItem}
                               </a>
                                </Link>
