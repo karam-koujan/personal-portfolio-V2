e96a6b523179c8d95bbe1546218d11836b1c63a8
karam <karamkaku2000@gmail.com>
2022-05-19 00:53:46 +0200

    add humberger

--- components/common/layout/header/index.tsx
+++ components/common/layout/header/index.tsx
@@ -1,23 +1,39 @@
+import React from "react";
 import Link from "next/link";
 import Logo from "../../logo/";
 import Toggle from "../../dark-light-mode/themeToggle.tsx"
-
+import Hamburger from "../../hamburger/";
 const Header = ({nav})=>{
+	const [showNavBar,setShowNavBar] = React.useState(false);
 	return(
-		<header>
-		<nav className="mx-auto w-full max-w-screen-xl flex justify-between items-center py-5">
+		<header className="px-[2rem] md:px-[1rem]">
+		<nav className="mx-auto w-full max-w-6xl flex justify-between items-center py-5">
 			<Logo/>
-				<div className="flex gap-12 items-center">	
+       		<div className="flex gap-12 items-center md:gap-7 sm:hidden">	
 			  {nav.map((navItem,idx)=>(
 			  <Link href={`/#${navItem}`} key={idx}>                               
 <a className="text-color-secondary dark:text-color-third font-thin  uppercase text-[0.8rem] tracking-widest relative before:content-[''] after:content-[''] before:w-0  after:w-0 before:h-[6px] after:bg-color-secondary after:absolute after:h-[6px] before:bg-color-secondary before:absolute before:bottom-[-100%] after:bottom-[-100%] before:left-[50%] before:right-[50%] after:left-[50%] after:right-[50%] before:rounded-[20px] after:rounded-[20px] before:transition-all before:ease-in-out before:duration-500  after:transition-all after:ease-in-out after:duration-500  hover:before:w-[100%] hover:after:w-[50%] hover:before:left-0 " >
 				 {navItem}
                               </a>
                                </Link>
-		     ))}
+               ))}
                             <Toggle/>
-			       </div>
-	         </nav>
+		   </div>
+	     <Hamburger showNavBar={showNavBar} setShowNavBar={setShowNavBar} />		     
+	      </nav>
+              
+		     <div className="sm:flex w-full flex-row-reverse">	     
+			     <div className="sm:flex gap-7 items-center flex-col  hidden w-[25rem] py-7 justify-center bg-color-primary">	
+			  {nav.map((navItem,idx)=>(
+			  <Link href={`/#${navItem}`} key={idx}>                               
+<a className="text-color-secondary dark:text-color-third font-thin  uppercase text-[0.8rem] tracking-widest relative before:content-[''] after:content-[''] before:w-0  after:w-0 before:h-[6px] after:bg-color-secondary after:absolute after:h-[6px] before:bg-color-secondary before:absolute before:bottom-[-100%] after:bottom-[-100%] before:left-[50%] before:right-[50%] after:left-[50%] after:right-[50%] before:rounded-[20px] after:rounded-[20px] before:transition-all before:ease-in-out before:duration-500  after:transition-all after:ease-in-out after:duration-500  hover:before:w-[100%] hover:after:w-[50%] hover:before:left-0 " >
+				 {navItem}
+                              </a>
+                               </Link>
+               ))}
+                <Toggle/>
+		    </div>
+                </div>
  		</header>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,24 @@ module.exports = {
        "color-secondary":"#4b6cc1",
        "color-third":"#ababab",
        "color-white":"#ffffff"	     
+     },
+    screens:{
+      'sm': {'max':'640px'},
+      'md': {'max':'768px'},
+      'lg': {'max':'1024px'},
+      'xl': {'max':'1280px'},
+      '2xl': {'max':'1536px'}
+    },
+    extend: {
+       animation:{
+           'fade-in':'fadeIn ease-in 0.4s finite'
+	    },   
+	keyframes:{
+		fadeIn : {
+		'from':{opacity:0,transform:'translateY(-10%)'},
+		'to':{opacity:1,transform:'translateY(0)'}
+		}
+	}
      }	  
   },
   plugins: [],
