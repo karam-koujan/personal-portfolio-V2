5338336eadf4e2c58034a8a418bc5f69b32873de
karam koujan <62403426+karam-koujan@users.noreply.github.com>
2022-05-19 11:39:30 +0200

    add Introduction component

---
+++ components/introduction/index.tsx
@@ -0,0 +1,15 @@
+const Introduction = () => {
+  return (
+    <section>
+      <h1>Hey, I’m Karam</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
+        maiores nam provident sed qui vel nesciunt aspernatur laboriosam magnam,
+        at autem aperiam repudiandae iste ducimus fugiat veritatis hic fuga
+        vero!
+      </p>
+    </section>
+  );
+};
+
+export default Introduction;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import {parseMdFileToObj} from "../lib/";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { parseMdFileToObj } from "../lib/";
 import Layout from "../components/common/layout";
-const Home: NextPage = ({nav}) => {
+import Introduction from "../components/introduction";
+const Home: NextPage = ({ nav }) => {
   return (
-     <Layout nav={nav}>
-         hello
-     </Layout>
-  )
+    <Layout nav={nav}>
+      <Introduction />
+    </Layout>
+  );
+};
+export async function getStaticProps() {
+  const navData = await parseMdFileToObj(["content", "nav", "nav.md"]);
+  return {
+    props: {
+      nav: navData.data.nav
+    }
+  };
 }
-export async function getStaticProps(){
-	const navData = await parseMdFileToObj(["content","nav","nav.md"])
-	return {
-		props:{
-		  nav:navData.data.nav
-		}
-	}
-}
-export default Home
+export default Home;
