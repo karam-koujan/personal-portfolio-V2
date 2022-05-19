f89cf0dd212ce9319af8d14a43987e3ea08057ac
karam koujan <62403426+karam-koujan@users.noreply.github.com>
2022-05-19 14:21:57 +0200

    add animation to text and btn in introduction

--- components/introduction/index.tsx
+++ components/introduction/index.tsx
@@ -13,23 +13,25 @@ const Introduction = () => {
             typeSpeed={90}
           />
         </h1>
-        <p className="text-color-primary dark:text-color-third fadeIn">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
-          maiores nam provident sed qui vel nesciunt aspernatur laboriosam
-          magnam, at autem aperiam repudiandae iste ducimus fugiat veritatis hic
-          fuga vero!
-        </p>
-        <div className="mt-[1.5rem] w-fit flex gap-7 sm:flex-col sm:m-auto sm:gap-3 sm:mt-[2rem]">
-          <Link href="">
-            <a>
-              <Button>my resume</Button>
-            </a>
-          </Link>
-          <Link href="">
-            <a>
-              <Button>get in touch</Button>
-            </a>
-          </Link>
+        <div className={Styles.fadeIn}>
+          <p className={` text-color-primary dark:text-color-third  `}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
+            maiores nam provident sed qui vel nesciunt aspernatur laboriosam
+            magnam, at autem aperiam repudiandae iste ducimus fugiat veritatis
+            hic fuga vero!
+          </p>
+          <div className="mt-[1.5rem] w-fit flex gap-7 sm:flex-col sm:m-auto sm:gap-3 sm:mt-[2rem]">
+            <Link href="">
+              <a>
+                <Button>my resume</Button>
+              </a>
+            </Link>
+            <Link href="">
+              <a>
+                <Button>get in touch</Button>
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
