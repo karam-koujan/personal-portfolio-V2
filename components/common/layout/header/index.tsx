d39838ea91c4e988eb2b45a94f19cb379b1ac8af
karam koujan <62403426+karam-koujan@users.noreply.github.com>
2022-05-19 08:52:19 +0200

    add box shadow

--- components/common/layout/header/index.tsx
+++ components/common/layout/header/index.tsx
@@ -10,7 +10,7 @@ const Header = ({ nav }) => {
       <nav className="mx-auto w-full max-w-6xl flex justify-between items-center py-5">
         <Logo />
         <div className="flex gap-12 items-center md:gap-7 sm:hidden">
-          {nav.map((navItem, idx) => (
+          {nav.map((navItem: string, idx: number) => (
             <Link href={`/#${navItem}`} key={idx}>
               <a className="text-color-secondary dark:text-color-third font-thin  uppercase text-[0.8rem] tracking-widest relative before:content-[''] after:content-[''] before:w-0  after:w-0 before:h-[6px] after:bg-color-secondary after:absolute after:h-[6px] before:bg-color-secondary before:absolute before:bottom-[-100%] after:bottom-[-100%] before:left-[50%] before:right-[50%] after:left-[50%] after:right-[50%] before:rounded-[20px] after:rounded-[20px] before:transition-all before:ease-in-out before:duration-500  after:transition-all after:ease-in-out after:duration-500  hover:before:w-[100%] hover:after:w-[50%] hover:before:left-0 ">
                 {navItem}
@@ -27,8 +27,8 @@ const Header = ({ nav }) => {
           showNavBar ? "opacity-100 translate-y-0" : ""
         }`}
       >
-        <div className="sm:flex gap-7 items-center flex-col  hidden w-[25rem] py-7 justify-center bg-color-primary">
-          {nav.map((navItem, idx) => (
+        <div className="sm:flex gap-7 items-center flex-col  hidden w-[25rem] py-7 justify-center bg-color-white dark:bg-color-primary shadow-3xl">
+          {nav.map((navItem: string, idx: number) => (
             <Link href={`/#${navItem}`} key={idx}>
               <a className="text-color-secondary dark:text-color-third font-thin  uppercase text-[0.8rem] tracking-widest relative before:content-[''] after:content-[''] before:w-0  after:w-0 before:h-[6px] after:bg-color-secondary after:absolute after:h-[6px] before:bg-color-secondary before:absolute before:bottom-[-100%] after:bottom-[-100%] before:left-[50%] before:right-[50%] after:left-[50%] after:right-[50%] before:rounded-[20px] after:rounded-[20px] before:transition-all before:ease-in-out before:duration-500  after:transition-all after:ease-in-out after:duration-500  hover:before:w-[100%] hover:after:w-[50%] hover:before:left-0 ">
                 {navItem}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,37 @@
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",		
-	],
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-     colors:{
-       "color-primary":"#191a1b",
-       "color-secondary":"#4b6cc1",
-       "color-third":"#ababab",
-       "color-white":"#ffffff"	     
-     },
-    screens:{
-      'sm': {'max':'640px'},
-      'md': {'max':'768px'},
-      'lg': {'max':'1024px'},
-      'xl': {'max':'1280px'},
-      '2xl': {'max':'1536px'}
+    colors: {
+      "color-primary": "#191a1b",
+      "color-secondary": "#4b6cc1",
+      "color-third": "#ababab",
+      "color-white": "#ffffff"
+    },
+    screens: {
+      sm: { max: "640px" },
+      md: { max: "768px" },
+      lg: { max: "1024px" },
+      xl: { max: "1280px" },
+      "2xl": { max: "1536px" }
     },
     extend: {
-       animation:{
-           'fade-in':'fadeIn ease-in 0.4s finite'
-	    },   
-	keyframes:{
-		fadeIn : {
-		'from':{opacity:0,transform:'translateY(-10%)'},
-		'to':{opacity:1,transform:'translateY(0)'}
-		}
-	}
-     }	  
+      animation: {
+        "fade-in": "fadeIn ease-in 0.4s finite"
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0, transform: "translateY(-10%)" },
+          to: { opacity: 1, transform: "translateY(0)" }
+        }
+      },
+      boxShadow: {
+        "3xl": "rgb(0 0 0) 0px 13px 15px -13px"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
