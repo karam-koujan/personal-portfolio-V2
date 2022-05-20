05d094f87c7154666a94187eb7b2bb5b63af880b
karam koujan <62403426+karam-koujan@users.noreply.github.com>
2022-05-20 17:19:36 +0200

    add className prop to button component

--- components/common/button/index.tsx
+++ components/common/button/index.tsx
@@ -1,11 +1,12 @@
-import Styles from "./styles.module.css";
-
 interface ButtonI {
   children: React.ReactNode;
+  className?: string;
 }
-const Button = ({ children }: ButtonI) => {
+const Button = ({ children, className }: ButtonI) => {
   return (
-    <button className="bg-color-secondary text-color-white rounded-[60px] capitalize text-[1.2rem] py-[.9em] px-[1.7em] border-solid border-2 border-color-secondary transition-all duration-[1000] ease-in  hover:bg-transparent focus:bg-transparent hover:text-color-secondary  focus:text-color-secondary">
+    <button
+      className={`bg-color-secondary text-color-white rounded-[60px] capitalize text-[1.2rem] py-[.9em] px-[1.7em] border-solid border-2 border-color-secondary transition-all duration-[1000] ease-in  hover:bg-transparent focus:bg-transparent hover:text-color-secondary  focus:text-color-secondary ${className}`}
+    >
       {children}
     </button>
   );
