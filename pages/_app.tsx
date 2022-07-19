b990e7812021595fed8dfdc9b3af727d109f6e5b
karam koujan <karamkaku2000@gmail.com>
2022-07-19 02:09:03 +0200

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import {ThemeProvider} from "../components/common/dark-light-mode/themeContext.tsx";
-import Background from "../components/common/dark-light-mode/background.tsx";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(()=>{
+		ReactGA.initialize(process.env.NEXT_PUBLIC_ANALYTICS_ID!);
+       ReactGA.pageview(window.location.pathname + window.location.search);
+	},[])
 	return(
 		<ThemeProvider>
                         <Component {...pageProps} />
