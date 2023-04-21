6b378294566dba9f32fc69996454a5ce5fe1ccaa
karam-koujan <karamkaku2000@gmail.com>
2023-04-21 04:02:21 +0000

    Update 3 files

--- components/blog/articles.tsx
+++ components/blog/articles.tsx
@@ -20,69 +20,71 @@ import Title from "../../components/common/title";
 
 const Articles = ({ articles }: propsI) => {
   return (
-    <div className="container">
-      <Title isVisible={true}>Articles</Title>
-      <main className="pb-[4rem]">
-        <ArticleWrapper isVisible={true} className="gap-[5%] sm:gap-[2%]">
-          {articles.map(
-            (
-              {
-                title,
-                image,
-                placeholder,
-                introduction,
-                tags,
-                date,
-                link,
-                duration,
-              }: articleI,
-              idx: number
-            ) => (
-              <Wrapper key={idx}>
-                <Link href={link}>
-                  <a>
-                    <ArticleImg placeholderColor={placeholder}>
-                      <Image
-                        layout="responsive"
-                        height={60}
-                        width={100}
-                        src={image}
-                        alt={`${title} image`}
-                      />
-                    </ArticleImg>
-                    <TextWrapper>
-                      <ArticleTitle className="text-color-primary dark:text-color-white my-[1rem] font-bold">
-                        {title}
-                      </ArticleTitle>
-                      <ArticleText className="text-color-primary dark:text-color-white my-[1rem]">
-                        {introduction}
-                      </ArticleText>
-                      <ArticleMetaData>
-                        <TagWrapper>
-                          {tags.map((tag, idx) => (
-                            <Tag
-                              key={idx}
-                              className="bg-color-secondary text-color-white"
-                            >
-                              {tag}
-                            </Tag>
-                          ))}
-                        </TagWrapper>
-                        <Date className="text-color-primary dark:text-color-white">
-                          {date}
-                        </Date>
-                        <Duration className="text-color-primary dark:text-color-white">
-                          {duration}
-                        </Duration>
-                      </ArticleMetaData>
-                    </TextWrapper>
-                  </a>
-                </Link>
-              </Wrapper>
-            )
-          )}
-        </ArticleWrapper>
-      </main>
+    <div className="px-[2rem] md:px-[1rem]">
+      <div className="container">
+        <Title isVisible={true}>Articles</Title>
+        <main className="pb-[14rem]">
+          <ArticleWrapper isVisible={true} className="gap-[5%] sm:gap-[2%]">
+            {articles.map(
+              (
+                {
+                  title,
+                  image,
+                  placeholder,
+                  introduction,
+                  tags,
+                  date,
+                  link,
+                  duration,
+                }: articleI,
+                idx: number
+              ) => (
+                <Wrapper key={idx}>
+                  <Link href={link}>
+                    <a>
+                      <ArticleImg placeholderColor={placeholder}>
+                        <Image
+                          layout="responsive"
+                          height={60}
+                          width={100}
+                          src={image}
+                          alt={`${title} image`}
+                        />
+                      </ArticleImg>
+                      <TextWrapper>
+                        <ArticleTitle className="text-color-primary dark:text-color-white my-[1rem] font-bold">
+                          {title}
+                        </ArticleTitle>
+                        <ArticleText className="text-color-primary dark:text-color-white my-[1rem]">
+                          {introduction}
+                        </ArticleText>
+                        <ArticleMetaData>
+                          <TagWrapper>
+                            {tags.map((tag, idx) => (
+                              <Tag
+                                key={idx}
+                                className="bg-color-secondary text-color-white"
+                              >
+                                {tag}
+                              </Tag>
+                            ))}
+                          </TagWrapper>
+                          <Date className="text-color-primary dark:text-color-white">
+                            {date}
+                          </Date>
+                          <Duration className="text-color-primary dark:text-color-white">
+                            {duration}
+                          </Duration>
+                        </ArticleMetaData>
+                      </TextWrapper>
+                    </a>
+                  </Link>
+                </Wrapper>
+              )
+            )}
+          </ArticleWrapper>
+        </main>
+      </div>
     </div>
   );
 };
--- components/common/layout/header/index.tsx
+++ components/common/layout/header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Logo from "../../logo/";
 import Toggle from "../../dark-light-mode/themeToggle.tsx";
 import Hamburger from "../../hamburger/";
@@ -9,15 +10,16 @@ interface HeaderI {
 }
 const Header = ({ nav }: HeaderI) => {
   const [showNavBar, setShowNavBar] = React.useState(false);
+  const {} = useRouter();
   return (
     <header className="px-[2rem] md:px-[1rem]">
       <nav className="mx-auto w-full max-w-6xl flex justify-between items-center py-5">
         <Logo />
         <div className="flex gap-12 items-center md:gap-7 sm:hidden">
           {nav.map((navItem: string, idx: number) => (
-            <Link href={`/#${navItem}`} key={idx}>
+            <Link href={`/${navItem}`} key={idx}>
               <a className="text-color-secondary dark:text-color-white font-thin  uppercase text-[0.8rem] tracking-widest relative before:content-[''] after:content-[''] before:w-0  after:w-0 before:h-[6px] after:bg-color-secondary after:absolute after:h-[6px] before:bg-color-secondary before:absolute before:bottom-[-100%] after:bottom-[-100%] before:left-[50%] before:right-[50%] after:left-[50%] after:right-[50%] before:rounded-[20px] after:rounded-[20px] before:transition-all before:ease-in-out before:duration-500  after:transition-all after:ease-in-out after:duration-500  hover:before:w-[100%] hover:after:w-[50%] hover:before:left-0  focus:before:w-[100%] focus:after:w-[50%] focus:before:left-0 ">
-                {navItem}
+                {navItem.split("#").join("")}
               </a>
             </Link>
           ))}
@@ -33,9 +35,9 @@ const Header = ({ nav }: HeaderI) => {
           }`}
         >
           {nav.map((navItem: string, idx: number) => (
-            <Link href={`/#${navItem}`} key={idx}>
+            <Link href={`/${navItem}`} key={idx}>
               <a className="text-color-secondary dark:text-color-white font-thin  uppercase text-[0.8rem] tracking-widest relative transition linear duration-500 hover:text-color-primary focus:text-color-primary dark:hover:text-color-secondary dark:focus:text-color-secondary ">
-                {navItem}
+                {navItem.split("#").join("")}
               </a>
             </Link>
           ))}
